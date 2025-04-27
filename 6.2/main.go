7f0b29ec8f840785b0cfeaf85e9dd883b0a21f38
Haruto Suzuki <ra54283suzuki@gmail.com>
2025-04-27 21:19:56 +0900

    Create code for section 6.2

---
+++ 6.2/main.go
@@ -0,0 +1,15 @@
+// リスト6.2 Goによる簡単なWebサーバ
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("static")))
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("failed to start : ", err)
+	}
+}
