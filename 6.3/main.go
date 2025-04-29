2af0d79d5e4da25ce789b9aeab6f16a7b6250f48
Haruto Suzuki <ra54283suzuki@gmail.com>
2025-04-29 17:13:58 +0900

    feat: enable adding todo items

--- 6.3/main.go
+++ 6.3/main.go
@@ -14,6 +14,13 @@ func handleTodo(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, todoList)
 }
 
+func handleAdd(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	todo := r.Form.Get("todo")
+	todoList = append(todoList, todo)
+	handleTodo(w, r)
+}
+
 func main() {
 	todoList = append(todoList, "顔を洗う", "朝食を食べる", "歯を磨く")
 
@@ -21,6 +28,8 @@ func main() {
 		http.StripPrefix("/static/",http.FileServer(http.Dir("static"))))
 	
 	http.HandleFunc("/todo",handleTodo)
+
+	http.HandleFunc("/add", handleAdd)
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
